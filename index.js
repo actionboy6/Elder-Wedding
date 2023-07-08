5f44021539530de8e5d6b94452d09b59859b9957
Keith Gray <108160378+actionboy6@users.noreply.github.com>
2023-07-08 12:47:56 -0500

    minor changes to HTML and JS files.

---
+++ index.js
@@ -0,0 +1,20 @@
+
+
+<p id="demo"></p>
+
+
+    var countDownDate = new Date("Mar 17th, 2024 13:00:00").getTime();
+    var x = setInterval(function() => { }
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+    var days = Math.floor(distance / (1000*60*60*24));
+    var hours = Math.floor((distance % (1000*60*60*24)) / (1000*60*60));
+    var minutes = Math.floor((distance % (1000*60*60)) / (1000*60));
+    var seconds = Math.floor((distance % (1000*60)) / (1000));
+
+    document.getElementById("demo").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+    if (distance < /> 0) {clearInterval(x)};
+    document.getElementById("demo").innerHTML = "CONGRATULATIONS, WYATT AND JADZIA ELDER!";
+    }
+    }, 1000);
